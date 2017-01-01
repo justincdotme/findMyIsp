f8893386d486a1198e4503e3bd8d8ed829071958
Justin Christenson <info@justinc.me>
2017-01-01 15:17:47 -0800

    Reverted Revert "Refactored PageController to accomodated model factories."
    This reverts commit b5f117d690349c8e73458a47d7a92c31d678a527.

--- src/controller/PageController.php
+++ src/controller/PageController.php
@@ -1,8 +1,77 @@
 <?php namespace nearMe\controller;
 
-use nearMe\Lib\Controller;
+use findMyIsp\lib\ClientModelFactory;
+use findMyIsp\lib\Curl;
+use findMyIsp\lib\IspModelFactory;
+use findMyIsp\lib\ModelFactory;
+use findMyIsp\lib\Request;
+use findMyIsp\model\Client;
+use findMyIsp\model\Isp;
+use findMyIsp\lib\View;
+use findMyIsp\lib\Controller;
+
 
 class PageController extends Controller
 {
 
+
+    protected $_view;
+    protected $_client;
+    protected $_isp;
+    protected $_request;
+
+    public function __construct()
+    {
+        $this->_view = new View;
+        $this->_client = new Client;
+        $this->_request = new Request;
+        $this->_isp = new Isp($this->_client->getLocation());
+    }
+
+    /**
+     * Display the index page for the application.
+     * Include client IP, geographic location and ISP name.
+     *
+     * @return mixed
+     */
+    public function index()
+    {
+        $view = 'home';
+
+        $data['clientIp'] = $this->_client->getIp();
+        $data['clientIsp'] = $this->_client->getIsp();
+
+        return $this->_view->make($view, $data);
+    }
+
+    /**
+     * List nearby ISPs.
+     * Returns JSON (if Ajax) or HTML.
+     *
+     * @return mixed
+     */
+    public function show()
+    {
+        if($this->_request->isPost())
+        {
+            if($this->_request->isAjax())
+            {
+                $location = filter_input(INPUT_POST, 'location' ,FILTER_SANITIZE_STRING);
+                if(!is_null($location))
+                {
+                    $this->_isp = new Isp($location);
+                }
+                $list = $this->_isp->getIspJsonList();
+                return $this->jsonOut($list);
+            }
+            $view = 'list-no-js';
+
+            $data['clientIp'] = $this->_client->getIp();
+            $data['clientIsp'] = $this->_client->getIsp();
+            $data['list'] = $this->_isp->getIspList();
+
+            return $this->_view->make($view, $data);
+        }
+        return false;
+    }
 }
